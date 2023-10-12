845d3787926c1d782914b46cecc0826fcff0bcac
Luke Bernstein <lnbernstein13@gmail.com>
2023-10-12 18:06:27 -0400

    put password in environment variable

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
+# must run source .venv/bin/activate before running app.py
 
 from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
 
-uri = "mongodb+srv://lnb337:8nWKMQ2CbVFLHEMl@cluster0-stuy.5kw8pxw.mongodb.net/?retryWrites=true&w=majority"
+load_dotenv()
+
+token = os.environ.get("password")
+# print(token)
+
+uri = "mongodb+srv://lnb337:" + token + "@cluster0-stuy.5kw8pxw.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
@@ -14,5 +22,8 @@ except Exception as e:
     print(e)
 
 db = client['sample_supplies']
-collection = db['sales']
-print(collection.find_one())
+
+travels = db['travels']
+travels.insert_one({'name': 'Lily', 'country': 'USA', 'itinerary': ['NYC', 'LA', 'SF']})
+print(travels.find_one())
+print('pls')
